98183ea2cf21410c7d71b9dd7af80c4501f0f3e4
Twinkle Sahoo <twinklesahoo79470l@gmail.com>
2024-12-06 01:22:14 +0530

    api.php modified

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,6 @@ Route::apiResource('stories', StoryController::class);
 Route::apiResource('categories', CategoryController::class);
 
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
